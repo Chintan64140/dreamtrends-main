94049375fe9dbe6c6604ccfc83e3c618ac3a9be7
Chintan64140 <chintan6414@gmail.com>
2026-04-10 14:04:44 +0530

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -60,6 +60,7 @@ export default function Footer() {
                       {link.label}
                     </Link>
                   ))}
+                  
                 </div>
               </div>
             ))}
